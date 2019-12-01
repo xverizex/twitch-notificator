ea58d28a2ffb3f6e76fdb1c7b2e9b86cf5c746b6
Dmitrii <naidv88@gmail.com>
2019-12-02 03:25:50 +0500

    добавлена лицензия

--- config.h
+++
@@ -1,2 +0,0 @@
-
-#define WEBHOOK        1
--- server.h
+++ server.h
@@ -1,1 +1,20 @@
+/*
+ * twitch-bot - бот показывает сообщения от twitch в области уведомления
+ *
+ * Copyright (C) 2019 Naidolinsky Dmitry <naidv88@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY of FITNESS for A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------/
+ */
 void *handle_server ( void *data );
